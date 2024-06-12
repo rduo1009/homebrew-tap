23a8d8eb65f60b51c3957835f93462a862d96ae5
rduo1009 <robertduong09alt@gmail.com>
2024-06-12 15:34:26 +1000

    Add files via upload

---
+++ Casks/graphmatica.rb
@@ -0,0 +1,11 @@
+cask "graphmatica" do
+  version "2.4"
+  sha256 "f970b1b4f58654ef3284fbe75d47d3f917ce3d6d5fcaf94da403422238c25c65"
+
+  url "http://www.graphmatica.com/mac/Graphmatica#{version}.dmg"
+  name "Graphmatica"
+  desc "Easy-to-use equation plotter"
+  homepage "graphmatica.com"
+
+  app "graphmatica.app"
+end
