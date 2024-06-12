ec83aba2af872993f30d2f9e87d011774b8d6a1f
rduo1009 <robertduong09alt@gmail.com>
2024-06-12 15:27:09 +1000

    Update adobe-fonts-revealer.rb

--- adobe-fonts-revealer.rb
+++ adobe-fonts-revealer.rb
@@ -4,9 +4,7 @@ class AdobeFontsRevealer < Formula
   desc "Copy Adobe Fonts (OTF) to your Downloads directory (macOS)"
   homepage "https://github.com/kalaschnik/adobe-fonts-revealer"
   url "https://github.com/kalaschnik/adobe-fonts-revealer.git", :using => :git, :branch => "main"
-  version "0.0.0"
-
-  head "https://github.com/kalaschnik/adobe-fonts-revealer.git"
+  version "latest"
 
   depends_on "bash"
   depends_on "lcdf-typetools"
