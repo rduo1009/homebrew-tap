8feda2f4a8dbbf5f760b1a937dab05a229f9cacd
rduo1009 <robertduong09alt@gmail.com>
2024-07-12 17:57:01 +1000

    Update gupax.rb

--- Casks/gupax.rb
+++ Casks/gupax.rb
@@ -1,6 +1,5 @@
 cask "gupax" do
-  version "1.3.8"
-  sha256 "d27f4b05b88a9cc3ec528017d95e455cdc3efba2cb4e3ec7a0ff42b0046421a0"
+  version "1.3.9"
 
   url "https://github.com/hinto-janai/gupax/releases/download/v#{version}/gupax-v#{version}-macos-x64-bundle.tar.gz"
   name "Gupax"
