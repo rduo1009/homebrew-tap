bdbe4ea5e18e9fb1cff15a08d30a5bdc15a55d9a
rduo1009 <robertduong09alt@gmail.com>
2024-07-31 12:34:31 +1000

    Add amphetamine-power-protect

---
+++ Casks/amphetamine-power-protect.rb
@@ -0,0 +1,10 @@
+cask "amphetamine-power-protect" do
+  version "latest"
+
+  url "https://github.com/x74353/Amphetamine-Power-Protect/raw/main/DMG/Power%20Protect%20for%20Amphetamine.dmg"
+  name "Amphetamine Power Protect"
+  desc "Installer for Amphetamine Power Protect"
+  homepage "https://github.com/x74353/Amphetamine-Power-Protect"
+
+  app "Install Power Protect.pkg"
+end
