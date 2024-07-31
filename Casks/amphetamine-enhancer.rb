c648d4a64bb4f763dd28837b84c6f3d7bef11801
rduo1009 <robertduong09alt@gmail.com>
2024-07-31 12:27:18 +1000

    Add amphetamine-enhancer

---
+++ Casks/amphetamine-enhancer.rb
@@ -0,0 +1,10 @@
+cask "amphetamine-enhancer" do
+  version "latest"
+
+  url "https://github.com/x74353/Amphetamine-Enhancer/raw/master/Releases/Current/Amphetamine%20Enhancer.dmg"
+  name "Amphetamine Enhancer"
+  desc "A helper application for Amphetamine that enhances some of its features."
+  homepage "https://github.com/x74353/Amphetamine-Enhancer"
+
+  app "Amphetamine Enhancer.app"
+end
